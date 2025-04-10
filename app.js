ecb048ff2e4937e77a75ca4dfb1a0c511db9bd71
Nikhil Acharya <nikhilacharya6823@gmail.com>
2025-04-10 10:17:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const port = process.env.PORT || 3000;
 app.use(
   cors({
     origin: "https://intern-scheduling.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
