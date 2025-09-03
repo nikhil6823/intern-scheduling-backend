9c3f71f0f414ac89e5c9cfcd349d165a4fb49496
Nikhil Acharya <nikhilacharya6823@gmail.com>
2025-09-03 12:41:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/chatbot", async (req, res) => {
 
   try {
     const completion = await client.chat.completions.create({
-    model: "deepseek/deepseek-chat-v3-0324:free",
+    model: "deepseek/deepseek-chat-v3.1:free",
     messages: [
       {
         "role": "user",
